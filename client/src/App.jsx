a612eb179991863ef14e8afc3f73339cdeccc7bd
Baiju Kuamr Yadav <yadavbaiju2005@gmail.com>
2025-07-21 12:18:26 +0530

    Update App.jsx
    adding backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [expenses, setExpenses] = useState([]);
 
   const fetchExpenses = async () => {
-    const res = await axios.get("http://localhost:5000/api/expenses");
+    const res = await axios.get("https://expense-tracker-mern-backend-pzst.onrender.com");
     setExpenses(res.data);
   };
 
